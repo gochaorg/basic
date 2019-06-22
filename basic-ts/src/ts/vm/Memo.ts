8aabc00f36f8c1c6433e5fa386727e89a44ca97a
Kamnev Georgiy <nt.gocha@gmail.com>
2019-06-23 01:31:55 +0500

    listeners

--- basic-ts/src/ts/vm/Memo.ts
+++ basic-ts/src/ts/vm/Memo.ts
@@ -1,11 +1,46 @@
+/**
+ * Интерфейс подписки на изменения ячеек памяти
+ */
+export type MemoListener = ( varname:string, from:any, to:any )=>any
+
+/**
+ * Память VM
+ */
 export class Memo {
-    values: { [name :string]:any } = {}
+    /**
+     * Непочредственно значния
+     */
+    readonly values: { [name :string]:any } = {}
+
+    /**
+     * Подписчики на изменения значений памяти VM
+     */
+    readonly listeners: MemoListener[] = []
+
+    /**
+     * Чтение значения
+     * @param varname имя переменной
+     */
     read(varname:string):any {
         console.log(`debug read var ${varname}`)
         return this.values[varname]
     }
+
+    /**
+     * Запись значения памяти
+     * @param varname имя переменной
+     * @param value значение переменной
+     */
     write(varname:string, value:any) {
         console.log(`debug write var ${varname} = ${value}`)
+        const old = this.values[varname]
         this.values[varname] = value
+        for( let ls of this.listeners ){
+            ls( varname, old, value )
+        }
+    }
+
+    get varnames():string[] {
+        return Object.keys(this.values)
     }
 }
