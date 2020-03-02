3f07af2cfe165abd81dc52fcccde981dd339e6e8
Kamnev Georgiy <nt.gocha@gmail.com>
2020-03-02 23:58:57 +0500

    ignores

--- basic-s/src/test/scala/xyz/cofe/sparse/typed/TypedChainRule2.scala
+++ basic-s/src/test/scala/xyz/cofe/sparse/typed/TypedChainRule2.scala
@@ -18,12 +18,16 @@ object TypedChainRule2 {
   // mul ::= atom * atom
   // atom ::= number | ( exp ) | - exp
 
-  class AST( val begin:LPointer[CToken], val end:LPointer[CToken] ) extends Tok[LPointer[CToken]]
-  class NumLitteral( begin:LPointer[CToken], end:LPointer[CToken], val value:Num ) extends AST(begin,end) {
-    override def toString: String = s"NumLitteral value=${value.value}"
+  type POINTER=LPointer[CToken]
+  type PARSER[T <: Tok[POINTER]] = GR[POINTER,T]
+
+  trait AST extends Tok[POINTER] { def begin:POINTER }
+  class AAST( val begin:LPointer[CToken], val end:LPointer[CToken] ) extends Tok[LPointer[CToken]] with AST
+  class NumLitteral( begin:LPointer[CToken], end:LPointer[CToken], val value:Num ) extends AAST(begin,end) {
+    override def toString: String = s"NumLitteral(${value.value})"
   }
 
-  val atom : GR[LPointer[CToken],AST] = ptr => {
+  val numAst : PARSER[AST] = ptr => {
     val t = ptr.lookup(0)
     if( t.isEmpty ){
       None
@@ -37,43 +41,161 @@ object TypedChainRule2 {
     }
   }
 
-  val operatorToken = Chars.ctest(c => (c=='+' || c=='-' || c=='*' || c=='/') );
+  class SpecChar( val chr:POINTER ) extends AAST( chr, chr.move(1) )
+  object SpecChar extends Enumeration {
+    import scala.language.implicitConversions
+    implicit def valueToOpVal(x:Value):Val = x.asInstanceOf[Val]
+    protected case class Val(val txtConst:String)
+      extends super.Val
+      with PARSER[AST]
+    {
+      def apply(ptr:POINTER):Option[SpecChar]={
+        val t = ptr.lookup(0)
+        if( t.isEmpty ){
+          None
+        }else{
+          val tv = t.get
+          if( tv.text.equals(txtConst) ){
+            Some( new SpecChar(ptr) )
+          }else{
+            None
+          }
+        }
+      }
+    }
 
-  class Operator( begin:LPointer[CToken], end:LPointer[CToken], val op:CToken ) extends AST(begin,end){
+    val BrOpen  = Val("(")
+    val BrClose = Val("(")
+  }
+
+  object Op extends Enumeration {
+    protected case class Val(val litteral:String) extends super.Val
+
+    import scala.language.implicitConversions
+    implicit def valueToOpVal(x:Value):Val = x.asInstanceOf[Val]
+
+    val ADD = Val("+")
+    val SUB = Val("-")
+    val MUL = Val("*")
+    val DIV = Val("/")
+  }
+  val operatorToken = Chars.ctest(c => Op.values.filter(o=>o.litteral.length==1 && o.litteral.charAt(0)==c).size>0 );
+  class Operator( begin:LPointer[CToken], end:LPointer[CToken], val op:CToken ) extends AAST(begin,end){
     override def toString: String = s"${op.text}"
   }
-  val operator : GR[LPointer[CToken],Operator] = ptr => {
-    val _t = ptr.lookup(0)
-    if( _t.isEmpty ){
-      None
-    }else{
-      val t = _t.get
-      t.text match {
-        case "+" => Some(new Operator(ptr,ptr.move(1),t))
-        case "-" => Some(new Operator(ptr,ptr.move(1),t))
-        case "*" => Some(new Operator(ptr,ptr.move(1),t))
-        case "/" => Some(new Operator(ptr,ptr.move(1),t))
-        case _ => None
+  def operator(ops:Op.Value*):GR[LPointer[CToken],Operator] = {
+    require(ops!=null)
+    ptr => {
+      val _t = ptr.lookup(0)
+      if( _t.isEmpty ){
+        None
+      }else{
+        val t = _t.get.text
+        if( ops.exists(o => o.litteral.equals(t)) ){
+          Some(new Operator(ptr,ptr.move(1),_t.get))
+        }else{
+          None
+        }
       }
     }
   }
 
-  class SumOp( begin:LPointer[CToken], end:LPointer[CToken], val operator:Operator, val left:AST, val right:AST ) extends AST(begin, end){
-    override def toString: String = s"SumOp op=$operator left=$left right=$right"
+  def binaryOp[U <: AST]( op:PARSER[Operator], left:PARSER[AST], right:PARSER[AST], init:AST=>U, builder:(U,Operator,AST)=>U ) = {
+    new GR[POINTER,U] {
+      override def apply(ptr: POINTER): Option[U] = {
+        require(ptr!=null)
+        if(ptr.eof()){
+          None
+        }else{
+          val leftRes = left(ptr)
+          if( leftRes.isEmpty ){
+            None
+          }else{
+            var res : U = init(leftRes.get);
+            var p = leftRes.get.end
+            var stop = false
+            while( !stop ){
+              if( p.eof() ){
+                stop = true
+              }else {
+                val opRes = op(p)
+                if (opRes.isEmpty) {
+                  stop = true
+                }else{
+                  val riRes = right(opRes.get.end)
+                  if( riRes.isEmpty ){
+                    stop = true
+                  }else{
+                    res = builder( res, opRes.get, riRes.get )
+                    p = riRes.get.end
+                  }
+                }
+              }
+            }
+            Some(res)
+          }
+        }
+      }
+    }
+  }
+  class BinaryOp(val left:AST, val right:Option[(Operator,AST)]=None ) extends AAST( left.begin, right.map( _._2.end ).getOrElse( left.end ) ){
+    override def toString: String = {
+      if( right.isEmpty ){
+        left.toString
+      }else{
+        s"${right.get._1}( $left , ${right.get._2} )"
+      }
+    }
   }
 
-  val sumOp = (atom + operator + atom) ==> ((left,op,right)=>{ new SumOp(left.begin, right.end, op, left, right) })
+  class RProxy[U <: AST]( var target:PARSER[U]=null ) extends PARSER[U] {
+    override def apply(ptr: POINTER): Option[U] = {
+      require(ptr!=null)
+      val trgt = target
+      if( trgt==null ) throw new IllegalStateException("target not set")
+      trgt(ptr)
+    }
+  }
+
+  val expr = new RProxy[AST]()
+
+  val brOpen : PARSER[AST] = SpecChar.BrOpen
+  val brClose : PARSER[AST] = SpecChar.BrClose
+  val brAtom = brOpen + expr + brClose ==> ( (bOpen, e, bClose) => e  )
+  val atom = ( numAst | brAtom ) ==> ( e=>e )
+  val mulOp = binaryOp[AST]( operator(Op.MUL, Op.DIV), atom, atom,
+    i => new BinaryOp(i),
+    (left, o, right) => new BinaryOp(left,right=Some(o,right))
+  )
+  val sumOp = binaryOp[AST]( operator(Op.ADD, Op.SUB), mulOp, mulOp,
+    i => new BinaryOp(i),
+    (left, o, right) => new BinaryOp(left,right=Some(o,right))
+  )
+
+  expr.target = sumOp
 
   def main(args:Array[String]):Unit = {
-    val tokens = Tokenizer.tokens("12 + 45.56", List(number3, ws, operatorToken), null)
-    val tokenList = tokens.toList.filter( x=> !x.isInstanceOf[WS] )
-    tokenList.foreach( tok => println( s"tok $tok" ) )
+    def t1()= {
+      val tokens = Tokenizer.tokens("12 + 45.56 - 23.8", List(number3, ws, operatorToken), null)
+      val tokenList = tokens.toList.filter(x => !x.isInstanceOf[WS])
+      tokenList.foreach(tok => println(s"tok $tok"))
 
-    val tokPtr = new LPointer[CToken](0,tokenList)
+      val tokPtr = new LPointer[CToken](0, tokenList)
 
-    println( tokPtr.lookup(0))
-    println( atom(tokPtr) )
-    println( operator(tokPtr.move(1)))
-    println( sumOp(tokPtr) )
+      println(tokPtr.lookup(0))
+      println(numAst(tokPtr))
+      println(sumOp(tokPtr))
+    }
+
+    def t2()= {
+      val tokens = Tokenizer.tokens("1 * 2 + 3 * 4", List(number3, ws, operatorToken), null)
+      val tokenList = tokens.toList.filter(x => !x.isInstanceOf[WS])
+      tokenList.foreach(tok => println(s"tok $tok"))
+
+      val tokPtr = new LPointer[CToken](0, tokenList)
+      println( expr(tokPtr) )
+    }
+
+    t2
   }
 }
